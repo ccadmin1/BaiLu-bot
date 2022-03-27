a323c2d9f750d3fd8c6b45add2f00cd15959f10d
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-03-27 15:41:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/ccadmin1/BaiLu-bot /BaiLu-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /BaiLu-bot
 fi
-cd /EvaMaria
+cd /BaiLu-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
