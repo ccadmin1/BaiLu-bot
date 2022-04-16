b63fddca0287a099fc2c9d91cc4d5d063c61eb6b
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-16 13:15:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,7 +2,7 @@ import os
 import logging
 import random
 import asyncio
-from script import Script
+from Script import script
 from pyrogram import Client, filters
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
